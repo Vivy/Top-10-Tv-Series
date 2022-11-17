15b848c6178330747dc9b3c8dd7a475b19bd1587
Vivy <Tomitaviorel@yahoo.com>
2022-11-17 13:52:10 +0200

    picture at the top and some css

--- src/component/pyramid/pyramid.style.js
+++ src/component/pyramid/pyramid.style.js
@@ -1,21 +1,45 @@
 import styled from 'styled-components';
 
 export const Cell = styled.div`
-  align-self: center;
+  align-self: flex-end;
   text-align: center;
   grid-column: ${({ start, end }) => `${start}/${end}`};
+  padding:1rem;
 &:first-child {
+  background-color:red;
+  bottom:0;
+  display:flex;
+  justify-content:center;
+  position:relative;
+  margin: 0%;
+  font-size:1.2rem;
   &::before {
-    content: "👑"
+    content: "👑";
+    top:0px;
+    position: absolute;
   }}
 &:nth-child(2) {
+  position:relative;
+  background-color:red;
+  display:flex;
+  justify-content:center;
+  align-items:flex-end;
   &::before{
-    content: "🥈"
+    content: "🥈";
+    position: absolute;
+    top:-10px;
   }
 }
 &:nth-child(3) {
+  position:relative;
+  display:flex;
+  background-color:red;
+  justify-content:center;
+  align-items:flex-end;
   &::before{
-    content: "🥉"
+    content: "🥉";
+    position:absolute;
+    top: -10px;
   }
 }
 `
@@ -25,6 +49,7 @@ export const Pyramid = styled.div`
   grid-template-columns: repeat(7, 1fr);
   grid-template-rows: repeat(4, 1fr);
   justify-items: center;
+  align-content: flex-end;
+  clip-path: polygon(50% 0, 100% 100%, 0% 100%);
+  background-color: green;
   `
-
-
--- src/component/top10/top10.style.js
+++ src/component/top10/top10.style.js
@@ -5,6 +5,18 @@ export const Flex = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  background-color: aliceblue;
   padding: 10px;
   gap: 1rem;
+  h1 {
+    position: relative;
+    &::after {
+      content: " ";
+      background-image: url('./top.jpg');
+      background-size: cover;
+      height: 50px;
+      width: 50px;
+      position: absolute;
+    }
+  }
 `
